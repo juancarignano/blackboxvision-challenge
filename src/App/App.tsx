28861532d588cd0199d2badb38f27331654b7081
Juan Carignano <69175258+juancarignano@users.noreply.github.com>
2021-07-27 12:34:36 -0300

    1-Challenge blackboxvision
    TODO:
    -Que al seleccionar una respuesta marque si fue correcta/incorrecta
    -Boton al finalizar para volver a iniciar

---
+++ src/App/App.tsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+
+import api from "../question/api";
+import QuestionCard from "../question/components/QuestionCard";
+import {Question} from "../question/types";
+import Button from "../ui/controls/Button";
+
+import styles from "./App.module.scss";
+
+const App: React.FC = () => {
+  const [questions, setQuestions] = React.useState<Question[]>([]);
+  const [currentQuestion, setCurrentQuestion] = React.useState<number>(0);
+  const [points, setPoints] = React.useState<number>(0);
+  const [status, setStatus] = React.useState<"pending" | "resolved" | "finished">("pending");
+  const question = questions[currentQuestion];
+
+  function onAnswer(text: string) {
+    if (question.correct_answer == text) {
+      switch (question.type) {
+        case "boolean":
+          setPoints((points) => points + 5);
+          break;
+        case "multiple":
+          setPoints((points) => points + 10);
+          break;
+      }
+    }
+
+    if (currentQuestion + 1 == questions.length) {
+      setStatus("finished");
+    } else {
+      setCurrentQuestion((question) => question + 1);
+    }
+  }
+
+  React.useEffect(() => {
+    api.list().then((questions) => {
+      setQuestions(questions);
+      setStatus("resolved");
+    });
+  }, []);
+
+  if (status == "pending") {
+    return <span>Loading...</span>;
+  }
+
+  if (status == "finished") {
+    return <span>You earned {points} points</span>;
+  }
+
+  return (
+    <main className={styles.container}>
+      <QuestionCard
+        footer={`${question.category} - ${question.difficulty}`}
+        header={`${currentQuestion + 1}/ ${questions.length}`}
+      >
+        {unescape(question.question)}
+      </QuestionCard>
+      <nav className={styles.answers}>
+        {[...question.incorrect_answers, question.correct_answer]
+          .sort((a, b) => a.localeCompare(b))
+          .map((answer) => (
+            <Button key={answer} onClick={() => onAnswer(answer)}>
+              {answer}
+            </Button>
+          ))}
+      </nav>
+    </main>
+  );
+};
+
+export default App;
